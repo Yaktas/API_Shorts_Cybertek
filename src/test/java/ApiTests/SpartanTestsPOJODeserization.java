b9fbcb2e2b88df22cab271e3638588ce5720b0e6
abbos97 <abbosboev@gmail.com>
2020-08-02 09:25:22 -0400

    Serialization

--- src/test/java/ApiTests/SpartanTestsPOJODeserization.java
+++ src/test/java/ApiTests/SpartanTestsPOJODeserization.java
@@ -2,6 +2,7 @@ package ApiTests;
 
 import static io.restassured.RestAssured.*;
 
+import com.google.gson.Gson;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +29,31 @@ public class SpartanTestsPOJODeserization {
         assertEquals(spartan1.getId(),15);
         assertEquals(spartan1.getGender(),"Female");
         assertEquals(spartan1.getPhone(),1938695106);
+    }
+
+    @Test
+    public void gsonExample(){
+        Gson gson=new Gson();
+
+        String myJsonBody="{\n" +
+                "    \"id\": 15,\n" +
+                "    \"name\": \"Meta\",\n" +
+                "    \"gender\": \"Female\",\n" +
+                "    \"phone\": 1938695106\n" +
+                "}";
+
+        //Using gson method do de serialization our json body
+        Spartan spartanMeta=gson.fromJson(myJsonBody, Spartan.class);
+
+        System.out.println(spartanMeta.toString());
+
+        //serialization Java Object --> JSON body
+
+        Spartan spartan=new Spartan(101,"Mike","Male",321342123l);
+        //converting custom class to json (serialization)
+        String jsonBody=gson.toJson(spartan);
+
+        System.out.println(jsonBody);
     }
 }
 
